47b1bb5ba0d89759a3b4e9ba3f03c53ca562111b
iahays <iahays@ucdavis.edu>
2017-03-29 21:23:18 -0700

    changed checkin/checkout to just focus barcode input right away for scan. Added search functionality to History, with an advanced search. Searching is in it's alpha phase, needs to add more functionality.

--- public/app/controllers/historyCtrl.js
+++ public/app/controllers/historyCtrl.js
@@ -4,10 +4,74 @@ angular.module('historyController',['ui.bootstrap'])
 	app = this;
 	app.currentPage = 1;
 	app.pageSize = 10;
+	app.searchLimit = 0;
+
 	History.getHistory().then(function(data) {
 		console.log(data);
 		app.historyData = data.data.history;
 	});
+    // Function: Show more results on page
+    app.showMore = function(number) {
+        app.showMoreError = false; // Clear error message
+        // Run functio only if a valid number above zero
+        if (number > 0) {
+            app.pageSize = number; // Change ng-repeat filter to number requested by user
+        } else {
+            app.showMoreError = 'Please enter a valid number'; // Return error if number not valid
+        }
+    };
+
+    // Function: Show all results on page
+    app.showAll = function() {
+        app.pageSize = undefined; // Clear ng-repeat limit
+        app.showMoreError = false; // Clear error message
+    };
+
+	app.search = function(searchKeyword, number) {
+		if(searchKeyword) {
+			if(searchKeyword.length > 0) {
+				app.pageSize = 0;
+				$scope.searchFilter = searchKeyword;
+				app.pageSize = number;
+
+			} else {
+				$scope.searchFilter = undefined;
+				app.pageSize = 0;
+			}
+		} else {
+			$scope.searchFilter = undefined;
+			app.pageSize = 0;
+		}
+	};
+
+	app.clear = function() {
+		$scope.number = 'Clear';
+		app.pageSize = 0;
+		$scope.searchKeyword = undefined;
+		$scope.searchFilter = undefined;
+		app.showMoreError = false;
+	};
+
+
+	app.advancedSearch = function(searchByFirstName, searchByLastName, searchByEmail) {
+		if (searchByFirstName || searchByLastName || searchByEmail) {
+			$scope.advancedSearchFilter = {};
+			if (searchByFirstName) {
+				$scope.advancedSearchFilter.firstName = searchByFirstName;
+			}
+			if (searchByLastName) {
+				$scope.advancedSearchFilter.lastName = searchByLastName;
+			}
+			if (searchByEmail) {
+				$scope.advancedSearchFilter.email = searchByEmail;
+			}
+			app.searchLimit = undefined;
+		}
+	};
+
+	app.sortOrder = function(order) {
+		app.sort = order;
+	};
 
 })
 
