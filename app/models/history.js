8d9d4571cd7b747cad61fbe107fb2b3b74831ca6
Navpreet Kaur <nkgkaur@ucdavis.edu>
2017-05-20 20:01:30 -0700

    Fixed Edit User Validation bug

--- app/models/history.js
+++ app/models/history.js
@@ -6,23 +6,23 @@ var Schema = mongoose.Schema; // Assign Mongoose Schema function to variable
 // Define the mongoose schema for history
 // Each schema maps to a MongoDB collection and defines the shape of the documents within that collection
 var HistorySchema = new Schema({
-	product: { type: String, lowercase: true, required: true, default: ' ' },
+	product: { type: String, lowercase: true, required: true, default: '' },
 	barcode: { type: Number, required: true},	
-	checkedType: { type: String, required: true, default: ' '},
-	firstName: { type: String, lowercase: true, default: ' '},
-	lastName: { type: String, lowercase: true, default: ' ' },
-	email: {type: String, lowercase: true, default: ' ' },	
+	checkedType: { type: String, required: true, default: ''},
+	firstName: { type: String, lowercase: true, default: ''},
+	lastName: { type: String, lowercase: true, default: '' },
+	email: {type: String, lowercase: true, default: '' },	
 	date: { type: Date, required: true, default: Date},
 	description: { type: String, default: 'description'},
-	phoneNumber: { type: String, default: ' ' },
-	supervisorFirstName: { type: String, default: ' ' },
-	supervisorLastName: { type: String, default: ' ' },
-	supervisorEmail: { type: String, default: ' ' },
-	supervisorPhoneNumber: { type: String, default: ' ' },
-	title: { type: String, default: ' '},
-	department: { type: String, default: ' '}, 
-	location: { type: String, default: ' '},
-	chargeNumber: { type: String, default: ' ' }
+	phoneNumber: { type: String, default: '' },
+	supervisorFirstName: { type: String, default: '' },
+	supervisorLastName: { type: String, default: '' },
+	supervisorEmail: { type: String, default: '' },
+	supervisorPhoneNumber: { type: String, default: '' },
+	title: { type: String, default: ''},
+	department: { type: String, default: ''}, 
+	location: { type: String, default: ''},
+	chargeNumber: { type: String, default:  '' }
 
 });
 
